f1288e7f9a1775d55e1768f8bc5e74b9d64c739a
xrayd <72351211+xrayd@users.noreply.github.com>
2022-11-29 23:25:53 -0500

    basic watchdog working; Blynk polling updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cs1600_water_gun
+Code governing our water gun for the CS1600 final project! 
+
+# Requirements
+C++ code requires Adafruit Sleepy Dog library and Blynk.
--- blynk_connection/blynk_connection.ino
+++ blynk_connection/blynk_connection.ino
@@ -2,28 +2,27 @@
 #define BLYNK_TEMPLATE_ID "TMPLiGjOag8o"
 #define BLYNK_DEVICE_NAME "Water Gun Trigger"
 #define BLYNK_AUTH_TOKEN  "5dY6oh0BlhF6Q3s_A-WFy40wHrRq7RNO"
-
-
 #define BLYNK_PRINT Serial
-
 
 #include <SPI.h>
 #include <WiFi101.h>
 #include <BlynkSimpleWiFiShield101.h>
+#include <Adafruit_SleepyDog.h>  // https://github.com/adafruit/Adafruit_SleepyDog/blob/master/examples/BasicUsage/BasicUsage.ino
 
 char auth[] = BLYNK_AUTH_TOKEN;
-
 char ssid[] = "Brown-Guest";
 char pass[] = "";
 
 BlynkTimer timer;
 
-int value = 0;
+int value = -1;
+int prevval = -1;  // for tracking a "rising edge" of virtual button push
 
 // This function is called every time the Virtual Pin 0 state changes; VP0 connected to our shoot button!
 BLYNK_WRITE(V0)
 {
   // Save value of our button as a value
+  prevval = value;
   value = param.asInt();
 
   // Update state
@@ -50,6 +49,8 @@ void setup()
 
   // Setup a function to be called every second by Blynk timer
   timer.setInterval(1000L, myTimerEvent);
+
+  int countdownMS = Watchdog.enable(1000);  // enable watchdog with specified number of milliseconds before reset
 }
 
 void loop()
@@ -58,6 +59,23 @@ void loop()
   // TODO: connect this code with the rest of our implementation!
   Blynk.run();
   timer.run();
-  Serial.println(value);  // this println only shows the value of our shooting
+  if (detect_rise()){
+    Serial.println("Button pressed!");
+  }
+  Watchdog.reset();  // reset the watchdog
 }
+
+// returns if the button was pressed or not by detecting rising edge
+bool detect_rise()
+{
+  if (prevval == 0 && value == 1){
+    prevval = -1;  // since polling time of loop is larger than WiFi polling time, we don't want this to return True a bunch of times
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+
 
